502737f00c5f0bee161f3436737e327841e6fa22
NatalyLapina <sumrakn@gmail.com>
2018-12-13 08:05:08 +0300

    Подключение скрипта и удаление класса map--faded

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var announcements = [
+  {
+    author: 'img/avatars/user{{xx}}.png',
+    offer: 'palace, flat, house или bungalo',
+    location:
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  }
+];
+
+var mapAd = document.querySelector('.map');
+mapAd.classList.remove('map--faded');
+
